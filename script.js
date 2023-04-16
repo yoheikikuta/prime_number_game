1d0961e96558343b3227d11639826c4bba0580dd
yoheikikuta <diracdiego@gmail.com>
2023-04-16 07:51:57 +0000

    Refactor script.js to use JavaScript class for better modularity

--- script.js
+++ script.js
@@ -1,104 +1,112 @@
-const startButton = document.getElementById('start-button');
-const numberDisplay = document.getElementById('number-display');
-const primeButton = document.getElementById('prime-button');
-const compositeButton = document.getElementById('composite-button');
-const timerDisplay = document.getElementById('timer');
-const factorizationResult = document.getElementById('factorization-result');
+class PrimeGame {
+  constructor() {
+    this.startButton = document.getElementById('start-button');
+    this.numberDisplay = document.getElementById('number-display');
+    this.primeButton = document.getElementById('prime-button');
+    this.compositeButton = document.getElementById('composite-button');
+    this.timerDisplay = document.getElementById('timer');
+    this.factorizationResult = document.getElementById('factorization-result');
 
-const totalQuestions = 2;
-let currentQuestion = 0;
-let correctAnswers = 0;
-let timeoutId;
+    this.totalQuestions = 2;
+    this.currentQuestion = 0;
+    this.correctAnswers = 0;
+    this.timeoutId = null;
 
-function isPrime(num) {
-  if (num <= 1) return false;
-  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
-    if (num % i === 0) return false;
+    this.setupEventListeners();
   }
-  return true;
-}
 
-function getPrimeFactors(num) {
-  const factors = [];
-  for (let divisor = 2; num > 1; divisor++) {
-    while (num % divisor === 0) {
-      factors.push(divisor);
-      num /= divisor;
+  isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+      if (num % i === 0) return false;
     }
-  }
-  return factors;
-}
-
-function handleButtonClick(button, correct) {
-  clearTimeout(timeoutId);
-  primeButton.disabled = true;
-  compositeButton.disabled = true;
-
-  if (correct) {
-    correctAnswers++;
+    return true;
   }
 
-  if (button) {
-    button.classList.add(correct ? 'correct' : 'incorrect');
-  }
-
-  // Display prime factorization result
-  const number = parseInt(numberDisplay.textContent, 10);
-  const factors = getPrimeFactors(number);
-  factorizationResult.textContent = `${number} = ${factors.join(' × ')}`;
-
-  setTimeout(() => {
-    if (button) {
-      button.classList.remove('correct', 'incorrect');
-    }
-    factorizationResult.textContent = ''; // Clear prime factorization result display
-    startNextQuestion();
-  }, 1000);
-}
-
-primeButton.addEventListener('click', () => {
-  const number = parseInt(numberDisplay.textContent, 10);
-  handleButtonClick(primeButton, isPrime(number));
-});
-
-compositeButton.addEventListener('click', () => {
-  const number = parseInt(numberDisplay.textContent, 10);
-  handleButtonClick(compositeButton, !isPrime(number));
-});
-
-function showResults() {
-  numberDisplay.textContent = `結果: ${correctAnswers} / ${totalQuestions} 問正解`;
-  startButton.disabled = false;
-  startButton.textContent = 'もう一度挑戦';
-  correctAnswers = 0;
-  currentQuestion = 0;
-}
-
-function startNextQuestion() {
-  if (currentQuestion < totalQuestions) {
-    currentQuestion++;
-    const randomNumber = Math.floor(Math.random() * 9) + 2; // Random integer between 2 and 10
-    numberDisplay.textContent = randomNumber;
-    startButton.disabled = true;
-    primeButton.disabled = false;
-    compositeButton.disabled = false;
-
-    let timeLeft = 10;
-    timerDisplay.textContent = timeLeft;
-
-    // Update timer
-    timeoutId = setInterval(() => {
-      timeLeft--;
-      timerDisplay.textContent = timeLeft;
-
-      if (timeLeft === 0) {
-        clearTimeout(timeoutId);
-        handleButtonClick(null, false); // Treat as incorrect answer when time runs out
+  primeFactorization(num) {
+    const factors = [];
+    for (let divisor = 2; num > 1; divisor++) {
+      while (num % divisor === 0) {
+        factors.push(divisor);
+        num /= divisor;
       }
+    }
+    return factors;
+  }
+
+  handleButtonClick(button, correct) {
+    clearTimeout(this.timeoutId);
+    this.primeButton.disabled = true;
+    this.compositeButton.disabled = true;
+
+    if (correct) {
+      this.correctAnswers++;
+    }
+
+    if (button) {
+      button.classList.add(correct ? 'correct' : 'incorrect');
+    }
+
+    const number = parseInt(this.numberDisplay.textContent, 10);
+    const factors = this.primeFactorization(number);
+    this.factorizationResult.textContent = `${number} = ${factors.join(' × ')}`;
+
+    setTimeout(() => {
+      if (button) {
+        button.classList.remove('correct', 'incorrect');
+      }
+      this.factorizationResult.textContent = '';
+      this.startNextQuestion();
     }, 1000);
-  } else {
-    showResults();
+  }
+
+  setupEventListeners() {
+    this.primeButton.addEventListener('click', () => {
+      const number = parseInt(this.numberDisplay.textContent, 10);
+      this.handleButtonClick(this.primeButton, this.isPrime(number));
+    });
+
+    this.compositeButton.addEventListener('click', () => {
+      const number = parseInt(this.numberDisplay.textContent, 10);
+      this.handleButtonClick(this.compositeButton, !this.isPrime(number));
+    });
+
+    this.startButton.addEventListener('click', this.startNextQuestion.bind(this));
+  }
+
+  showResults() {
+    this.numberDisplay.textContent = `結果: ${this.correctAnswers} / ${this.totalQuestions} 問正解`;
+    this.startButton.disabled = false;
+    this.startButton.textContent = 'もう一度挑戦';
+    this.correctAnswers = 0;
+    this.currentQuestion = 0;
+  }
+
+  startNextQuestion() {
+    if (this.currentQuestion < this.totalQuestions) {
+      this.currentQuestion++;
+      const randomNumber = Math.floor(Math.random() * 9) + 2;
+      this.numberDisplay.textContent = randomNumber;
+      this.startButton.disabled = true;
+      this.primeButton.disabled = false;
+      this.compositeButton.disabled = false;
+
+      let timeLeft = 10;
+      this.timerDisplay.textContent = timeLeft;
+
+      this.timeoutId = setInterval(() => {
+        timeLeft--;
+        this.timerDisplay.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+          clearTimeout(this.timeoutId);
+          this.handleButtonClick(null, false);
+        }
+      }, 1000);
+    } else {
+      this.showResults();
+    }
   }
 }
 
-startButton.addEventListener('click', startNextQuestion);
+const game = new PrimeGame();
